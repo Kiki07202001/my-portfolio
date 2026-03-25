3ece03001d372cddc2e147b5790553ef2bd32e5e
Kiki Brusey <kikibrusey@Kikis-MacBook-Air.local>
2026-03-24 21:27:12 -0400

    Dynamic Project Gallery

---
+++ script.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll(".details-btn");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+
+        const details = this.nextElementSibling;
+
+        if (details.style.display === "none" || details.style.display === "") {
+            details.style.display = "block";
+            this.textContent = "Hide Details";
+        } else {
+            details.style.display = "none";
+            this.textContent = "Show Details";
+        }
+
+    });
+});
